3ae43bd1b9c316506600751fd2525333f2d9628b
Roberto Torga <rtorga@gmail.com>
2020-03-26 10:59:22 -0300

    Frontend da aplicacao

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,6 +17,6 @@ routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/profiles', ProfileController.index);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
